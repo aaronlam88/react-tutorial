640847247df4965a550cbe6299d41bd204655a92
aaronlam88 <aaron.y.lam@sjsu.edu>
2019-10-31 22:11:02 -0700

    update

--- client/src/pages/app/App.js
+++ client/src/pages/app/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from '../../images/logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import Login from '../login/Login';
+import About from '../about/About';
+import Users from '../users/Users';
+
+// import logo from "../../images/logo.svg";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about/">About</Link>
+          </li>
+          <li>
+            <Link to="/users/">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Route path="/" exact component={Login} />
+      <Route path="/about/" component={About} />
+      <Route path="/users/" component={Users} />
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;
---
+++ client/src/pages/users/Users.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Users = () => <h2>Users</h2>;
+
+export default Users;
