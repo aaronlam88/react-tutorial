00a42d8093c8ccd1fb7f6e1215095b6eba9b0d74
aaronlam88 <aaron.y.lam@sjsu.edu>
2019-10-31 22:04:59 -0700

    setup for vscode

--- client/src/pages/index/Index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Index = () => {
-  return <h2>Index</h2>;
-};
-
-export default Index;
--- client/src/pages/users/Users.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Users = () => {
-  return <h2>Users</h2>;
-};
-
-export default Users;
