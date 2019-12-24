6a410115e915a2b0519f2f1f701297b6452e9fa9
aaronlam88 <caaronlam.swe@gmail.com>
2019-12-24 10:34:28 -0800

    fix format

--- client/src/pages/app/App.jsx
+++ client/src/pages/app/App.jsx
@@ -1,35 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Home from '../home/Home';
+import ErrorBoundary from '../../components/error-boundary/ErrorBoundary';
+
+import Login from '../login/Login';
 import About from '../about/About';
 import Users from '../users/Users';
+import ErrorPage from '../error-page/ErrorPage';
 
 // import logo from "../../images/logo.svg";
 import './App.css';
 
 const App = () => (
-  <Router>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about/">About</Link>
-          </li>
-          <li>
-            <Link to="/users/">Users</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Route path="/" exact component={Home} />
-      <Route path="/about/" component={About} />
-      <Route path="/users/" component={Users} />
-    </div>
-  </Router>
+  <ErrorBoundary>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
+        <Route path="/error" component={ErrorPage} />
+      </Switch>
+    </Router>
+  </ErrorBoundary>
 );
 
 export default App;
