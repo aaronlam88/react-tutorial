05990749e4804ddc6e05956fd41949561e15eab9
aaronlam88 <caaronlam.swe@gmail.com>
2020-06-29 00:00:16 -0700

    update: add eslint for server

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
