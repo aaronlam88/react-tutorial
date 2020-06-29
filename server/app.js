fca9b3807c9788e985fbd93793b1d64d68e541b2
aaronlam88 <aaron.y.lam@sjsu.edu>
2020-06-28 17:14:59 -0700

    move express to es6

--- server/app.js
+++ server/app.js
@@ -1,30 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import createError from 'http-errors';
+import express from 'express';
+import helmet from 'helmet';
+import logger from 'morgan';
+import path, { join } from 'path';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
 
-var app = express();
+const app = express();
+const __dirname = path.resolve();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// adding Helmet to enhance your API security
+app.use(helmet());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(logger('combined'));
+
+// Content-Type parsing
+// parse application/json
+app.use(bodyParser.json());
+// parse text/plain
+app.use(bodyParser.text());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// add cookie parser
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// add public directory
+app.use(express.static(join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -34,4 +52,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// add routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+export default app;
