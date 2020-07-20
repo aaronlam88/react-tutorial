42a8251fb656249d248dc7d529dadc1de6448dbd
aaronlam88 <caaronlam.swe@gmail.com>
2020-07-19 20:21:46 -0700

    add jest test framework

--- server/src/utils/adapters/mysql/adapter.js
+++ server/src/utils/adapters/mysql/adapter.js
@@ -1,21 +1,48 @@
+
+/**
+ * This object will handle connection from mysql database.
+ * Required the following environment variables: 
+ * - MYSQL_HOST: mysql database hostname
+ * - MYSQL_PORT: mysql database port number
+ * - MYSQL_USER: mysql username
+ * - MYSQL_PASS: mysql password
+ * Extra setting can be supplied by setting.json file
+ */
 import mysql from 'mysql';
-import dotenv from 'dotenv'
+
 // import setting from setting.json file
 import setting from './setting.json';
-// set env variables with .env file
-dotenv.config();
 
-const { connectionLimit, queueLimit } = setting;
+// setup log
+import logger from '../../log/winston_logger.js';
+
+// setup mysql connection variables with environment variables
 const host = process.env.MYSQL_HOST;
+const port = process.env.MYSQL_PORT;
 const user = process.env.MYSQL_USER;
 const password = process.env.MYSQL_PASS;
 
+/**
+ * Create mysql connection pool, see https://www.npmjs.com/package/mysql
+ */
 const pool = mysql.createPool({
   host,
+  port,
   user,
   password,
-  connectionLimit,
-  queueLimit,
+  ...setting
+});
+
+pool.on('acquire', function (connection) {
+  logger.debug('Connection %d acquired', connection.threadId);
+});
+
+pool.on('connection', (connection) => {
+  logger.debug('new connection %d', connection.threadId);
+});
+
+pool.on('enqueue', function () {
+  logger.debug('Waiting for available connection slot');
 });
 
 export default pool;
