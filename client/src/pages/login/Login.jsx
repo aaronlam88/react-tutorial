b2a52101024c1070a37a9b18eec963f64bb0d251
aaronlam88 <aaronlam.swe@gmail.com>
2020-07-31 16:36:41 -0700

    basic setup for express js

--- client/src/pages/login/Login.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Login = () => <h2>Login</h2>;
-
-export default Login;
--- client/src/pages/users/Users.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Users = () => <h2>Users</h2>;
-
-export default Users;
